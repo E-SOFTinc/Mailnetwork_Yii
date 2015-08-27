c15ecd0eca3668a059ef3336fb5ded99e4a29e17
Yanndc <yann@e-soft.ca>
2015-08-27 17:34:37 -0400

    envoi initial. premiere partie.

---
+++ db/001_init_database.sql
@@ -0,0 +1,2 @@
+create database mailnetwork character set utf8; 
+grant all privileges on mailnetwork.* to 'mailnetwork'@'localhost' identified by 'F1B99D9548017DCD15919D9652';
---
+++ db/002_reload_database.sql
@@ -0,0 +1,2 @@
+drop database if exists mailnetwork;
+create database mailnetwork;
